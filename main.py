7a9c41084d8321a151d4103d2de7b6dcf02cccbf
Juan Lotito <juan.lotito@unidadeditorial.es>
2020-10-25 20:12:34 +0100

    primera subida pasando los argumentos

--- main.py
+++ main.py
@@ -4,14 +4,54 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 import os
+import socket
+import argparse
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import messaging
 
 import registrationCodes
 
+
+def parse_arguments():
+    # Create argument parser
+    parser = argparse.ArgumentParser()
+
+    # Positional mandatory arguments
+    parser.add_argument("name", help="File Name", type=str)
+    parser.add_argument("size", help="File Size", type=str)
+    parser.add_argument("active_time", help="File Active Time", type=str)
+    parser.add_argument("full_path", help="File Full Path", type=str)
+
+    # Optional arguments
+    parser.add_argument("-d", "--debug", help="Debug Mode", type=bool, default=False)
+
+    # Print version
+    parser.add_argument("--version", action="version", version='%(prog)s - Version 1.0')
+
+    # Parse arguments
+    args = parser.parse_args()
+
+    return args
+
+
+def is_host_open(ip, port):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.settimeout(3)
+    try:
+        # s.connect((ip, int(port)))
+        s.connect(("192.168.1.40", int(port)))
+        s.shutdown(socket.SHUT_RDWR)
+        return True
+    except:
+        return False
+    finally:
+        s.close()
+
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
+    args = parse_arguments()
     path = os.path.dirname(os.path.abspath(__file__))
 
     # This registration token comes from the client FCM SDKs.
@@ -22,8 +62,8 @@ if __name__ == '__main__':
 
     message = messaging.Message(
         notification=messaging.Notification(
-            title='Title 12',
-            body='Content 12'
+            title='Descarga Completada',
+            body='File: ' + args.name
         ),
         android=messaging.AndroidConfig(
             notification=messaging.AndroidNotification(
